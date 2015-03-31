cef45304435d4130f5b164a91d6a76266b4d8de8
Lameaux <sergii.sizov@gmail.com>
2015-03-31 11:00:50 +0200

    get free space

--- src/main/java/com/euromoby/utils/SystemUtils.java
+++ src/main/java/com/euromoby/utils/SystemUtils.java
@@ -1,9 +1,15 @@
 package com.euromoby.utils;
 
+import java.io.File;
+
 public class SystemUtils {
 
 	public static String getUserHome() {
 		return System.getProperty("user.home");
 	}
 
+	public static long getFreeSpace(String path) {
+		return new File(path).getFreeSpace();
+	}
+	
 }
