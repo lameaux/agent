9431b6fb5270ea79f13d244df3eacfdb1bf06e6e
Sergii Sizov <sergii.sizov@gmail.com>
2015-04-01 21:48:23 +0200

    certificate - valid forever

--- bin/keystoregen.bat
+++ bin/keystoregen.bat
@@ -1,7 +1,7 @@
 rem Generate certificate and private key
 openssl genrsa -out agent.key 2048
 openssl req -new -out agent.csr -key agent.key
-openssl x509 -req -days 365 -in agent.csr -signkey agent.key -out agent.crt
+openssl x509 -req -days 5000 -in agent.csr -signkey agent.key -out agent.crt
 
 rem Create PKCS12 keystore (use password 123456)
 openssl pkcs12 -export -in agent.crt -inkey agent.key -out agent.p12 -name agent
