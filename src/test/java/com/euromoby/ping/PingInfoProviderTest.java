24595e7188e1a555c48ecdd8a2d24bd3981e0abd
Sergii Sizov <sergii.sizov@gmail.com>
2015-04-02 21:10:04 +0200

    PingInfoProviderTest, formatting

---
+++ src/test/java/com/euromoby/ping/PingInfoProviderTest.java
@@ -0,0 +1,38 @@
+package com.euromoby.ping;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.euromoby.agent.Config;
+import com.euromoby.model.AgentId;
+import com.euromoby.model.PingInfo;
+import com.euromoby.utils.SystemUtils;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PingInfoProviderTest {
+	
+	@Mock
+	Config config;
+	PingInfoProvider pingInfoProvider;
+
+	@Before
+	public void init() {
+		pingInfoProvider = new PingInfoProvider(config);
+	}
+	@Test
+	public void testCreatePingInfo() {
+		AgentId agentId = new AgentId("host:12345");
+		Mockito.when(config.getAgentId()).thenReturn(agentId);
+		Mockito.when(config.getAgentFilesPath()).thenReturn(SystemUtils.getUserHome());
+		PingInfo pingInfo = pingInfoProvider.createPingInfo();
+		assertEquals(agentId, pingInfo.getAgentId());
+		assertEquals(SystemUtils.getFreeSpace(SystemUtils.getUserHome()), pingInfo.getFreeSpace());
+	}
+
+}
